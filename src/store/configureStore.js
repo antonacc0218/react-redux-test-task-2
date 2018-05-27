2c3d10d1a25966ef0e09ada86d8e1baa15895a23
artyom.bochkov <forbocha@gmail.com>
2018-05-27 10:41:07 +0300

    exclude devTools from production

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,13 +5,13 @@ import { loadState, saveState } from './sessionStorage';
 
 const initialState = loadState();
 
-const createStoreWithMiddleware = compose(
-  applyMiddleware(thunk)
-)(createStore);
-
-const store = createStoreWithMiddleware(rootReducer, initialState,
+const enhancer = compose(
+  applyMiddleware(thunk),
+  process.env.NODE_ENV !== 'production' &&
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+const store = createStore(rootReducer, initialState, enhancer);
 
 store.subscribe(() => {
   saveState({ user: { id: store.getState().user.id } });
