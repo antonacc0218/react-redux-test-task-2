643786ae6b4e61796f950ef3b5e46fea2bbc3af7
artyom.bochkov <forbocha@gmail.com>
2018-05-29 08:56:04 +0300

    fixed bug of refresh page on the prod version

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 // TODO: replace it by more lightweight server framework or implement manually.
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.static(`${__dirname}/dist`));
 
 const PORT = process.env.PORT || 8081;
+
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is up and running. Port: ${PORT}`);
